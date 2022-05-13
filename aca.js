3111e3359c548e1059736f8da502605407728d78
HarutyunHakhverdyan <92093271+HarutyunHakhverdyan@users.noreply.github.com>
2022-05-13 07:40:28 +0400

    Add files via upload

--- aca.js
+++ aca.js
@@ -1,126 +1,111 @@
-
-//1 Given an array. Write a recursive function that removes the first element and returns the given
-//array. (without using arr.unshift(),assign second element to first, third element to second...)
-
-function funcShift(array,i=0){
-    arr[i]=arr[i+1];
-    if(i==array.length-1){
-        array.length=array.length-1
-        i=0
-        return array
-      } 
-    if(i!=array.length-1){
-        i++
-        funcShift(array,i)
-    };
-    return array
-};
-let arr=[1,2,3,5,'k','j',9]
-let result = funcShift(arr);
-console.log(result)
-
-// 2. Given an array of nested arrays. Write a recursive function that flattens it. (Hint create function
-//that concats arrays).
- 
- function arrConcat(arr,i=0,arr2=[]){
-    //debugger
-    if(i!=arr.length){
-    if(typeof(arr[i])=='object'){
-     for(let j=0;j<arr[i].length;j++){
-        if(typeof(arr[i][j])=='object'){
-            arrConcat(arr[i][j],0,arr2)
-        }else{
-        arr2.push(arr[i][j])
-        }
-     } 
-     i++;
-     arrConcat(arr,i,arr2) 
-    }else{
-      arr2.push(arr[i]);
-      i++;
-      arrConcat(arr,i,arr2)
+/*
+//2. Write a JavaScript function to get all possible subsets of given length of the given
+//array.
+ function a(arr,n,newArr=[],firstI=0,nextI=1,curentI=2){    
+  if(newArr.length!=n){
+    newArr[0]=arr[firstI];
+  }
+  if(newArr.length!=n){
+    newArr[1]=arr[nextI]
+  }
+  if(newArr.length!=n){
+    newArr.push(arr[curentI])
+  }
+  if(newArr.length==n){
+    console.log(newArr)
+    if(curentI!=arr.length-1){
+      curentI=arr.indexOf(newArr[2])
     }
+    if(arr.indexOf(newArr[1])==-1){
+      nextI=arr.length
+    }
+    newArr=[]
+  }
+   if(curentI==-1){
+    curentI=arr.length
+  }
+   if(curentI<arr.length-1){
+    curentI++;
+    a(arr,n,newArr,firstI,nextI,curentI)
+  }else if(nextI<arr.length-n+1){
+    nextI++;
+    a(arr,n,newArr,firstI,nextI,nextI+1)
+  }else if(arr.length>n){
+    arr.shift();
+    a(arr,n)
+  }
+ }
+
+ a([1,2,3,4],1)
+//3. Write a recursive function to determine whether all digits of the number are odd or
+//not.
+function odd(num){
+  if((num%10)%2==1){
+    num =(num-num%10)/10
+    return odd(num)
+  }
+  return num==0
+};
+let result=odd(7);
+console.log(result)
+
+//4 Given an array of numbers. Write a recursive function to find its minimal positive element. (if
+//such element does not exist, return -1)․
+function minPositiv(arr,i=0,minEl=-1){
+   if(i!=arr.length){
+     if(arr[i]>=0){
+       if(arr[i]<minEl||minEl<0){
+      minEl=arr[i]
+      i++
+       return minPositiv(arr,i,minEl)
+     }
    }
-return arr2
- };
- let arr1=[1,[[123123],3,7,8],6,[98],[2,[4,[8]]]];
- let result= arrConcat(arr1);
- console.log(result)
-
-// 3 Given an array and a number N.  Write a recursive function that rotates an array N places to the
-//left. (Hint: to add element to the beginning use arr.unshift())
-function arrRotat(arr,N){
-    if(N>0){
-        arr.push(arr.shift())
-        N--
-        arrRotat(arr,N)
-    };
-    return arr
+     i++
+     return minPositiv(arr,i,minEl)
+ }
+ return minEl
 };
-let arr1 = [1,2,3,4,5,6,7];
-let result = arrRotat(arr1,2);
+let result=minPositiv([-1,-2,-3,-2,-4,-5,6]);
 console.log(result)
-
-/* 4. Given the list of the following readers:
- [
-{ book:'Catcher in the Rye', readStatus: true, percent: 40},
-{ book:'Animal Farm', readStatus: true, percent: 20},
-{ book:'Solaris', readStatus: false, percent: 90 },
-{ book:'The Fall', readStatus: true, percent: 50 },
-{ book:'White Nights', readStatus: false, percent: 60 } ,
-{ book:'After Dark', readStatus: true, percent: 70 }
-];
-Output the books sorted by the percent in descending order which readStatus is true.
 */
-let arr1 = [
-{ book:'Catcher in the Rye', readStatus: true, percent: 40},
-{ book:'Animal Farm', readStatus: true, percent: 20},
-{ book:'Solaris', readStatus: false, percent: 90 },
-{ book:'The Fall', readStatus: true, percent: 50 },
-{ book:'White Nights', readStatus: false, percent: 60 } ,
-{ book:'After Dark', readStatus: true, percent: 70 }
-];
-function sortBooks(arr){
-    arr=arr.filter(function(el){
-        if(el.readStatus==true){
-            return true
-        }
-    }) 
-    arr=arr.sort(function(a,b){
-            if(a.percent>b.percent){
-                return -1
-            }
-        })
-    return arr
-};
-let result = sortBooks(arr1);
-console.log(result)
-
-// 5 Implement “map” method for plain objects (same as Array map method.);
-
-function mapForObj(fn,thisValue){
-    let newObj={}
-    fn.apply()
-          for(let k in this){
-            if(k!='mapForObj'){
-              if(thisValue){
-            newObj[k] = fn.apply(thisValue,[this[k],k,this])
-             } else {
-            newObj[k]=fn(this[k],k,this)
-          }
-        }
+function treeObj(arr,i=0,obj={}){
+    if(i!=arr.length){
+      let {parent:p,id}=arr[i];
+      if(p==null){
+        obj[id]={}
       }
-   return newObj  
+      function objRec(obj,p,id,t=1){
+        for(let key1 in obj){
+          if(key1==p){
+            obj[key1][id]={}
+            t=0
+          }
+          for(let key2 in obj[key1]){
+            if(p==key2){
+              obj[key1][key2][id]={}
+              t=0
+            }
+          }
+        
+        if(t){
+          objRec(obj[key1],p,id)
+        }
+       }
+      }
+     objRec(obj,p,id)
+      i++
+      treeObj(arr,i,obj)
+    }
+    return obj
 };
-let obj3={
-    name:'Haik'
-}
-let obj1={
-    name1:'Suren',
-    name2:'Garik'
-}
-obj1['mapForObj']=mapForObj;
-let result=obj1.mapForObj(function(value,key,obj){
-    return this.name+'anush'
-},obj3)
+let result=treeObj( [
+{ parent: null, id: 0 },
+{parent: 0, id: 1 },
+{parent: 0, id: 2 }, 
+{parent: 1, id: 3 },
+{parent: 1, id: 4 },
+{parent: 2, id: 5 },
+{parent: 4, id: 6 },
+{parent: 5, id: 7 }
+]);
 console.log(result)
